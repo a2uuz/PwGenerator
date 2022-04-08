11455b37a93a0330f201f20d620ab69c6cf44d3b
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-04-08 23:11:51 +0300

    update

--- script.js
+++ script.js
@@ -1,44 +1,71 @@
     let pw = document.getElementById("password");
-    let pw2 = document.getElementById("passwordT");
+    let pw2 = document.getElementById("password2");
+    let pw3 = document.getElementById("password3");
+    let pw4 = document.getElementById("password4");
     let btn = document.querySelector("#button");
+    
 
-    btn.addEventListener("click", ()=>{
-     let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = 12;
 
-    let passwordT = "";
-    for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    passwordT += chars.substring(randomNumber, randomNumber+1);
-    }
-    pw2.value = passwordT;
-    })
-   
-   
-   
-   
+
+
+    btn.addEventListener('click', genPassword)
+    btn.addEventListener('click', genPassword2)
+    btn.addEventListener('click', genPassword3)
+    btn.addEventListener('click', genPassword4)
+
+
+
+
+    
 function genPassword() {
 let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let passwordLength = 12;
-
 let password = "";
+
 for (var i = 0; i <= passwordLength; i++) {
 var randomNumber = Math.floor(Math.random() * chars.length);
 password += chars.substring(randomNumber, randomNumber+1);
 }
+
 pw.value = password;
 }
 
+function genPassword2() {
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let passwordLength = 12;
+let password = "";
 
-
-
-
-
-
-    function copyPassword() {
-  var copyText = document.getElementById("password");
-  copyText.select();
-  copyText.setSelectionRange(0, 999);
-  document.execCommand("copy");
-  
+for (var i = 0; i <= passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * chars.length);
+password += chars.substring(randomNumber, randomNumber+1);
 }
+
+pw2.value = password;
+}
+
+function genPassword3() {
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let passwordLength = 12;
+let password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * chars.length);
+password += chars.substring(randomNumber, randomNumber+1);
+}
+
+pw3.value = password;
+}
+
+function genPassword4() {
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let passwordLength = 12;
+let password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * chars.length);
+password += chars.substring(randomNumber, randomNumber+1);
+}
+
+pw4.value = password;
+}
+
